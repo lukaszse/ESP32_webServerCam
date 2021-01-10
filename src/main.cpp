a39b5c29c3c66336e3c37c9f98823e51eea70f45
lukaszse <llseremak@gmail.com>
2021-01-10 09:51:40 +0100

    nezt version

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <WiFiClient.h>
-
 #include <WiFi.h>
 #include <ESPmDNS.h>
 #include <esp_camera.h>
@@ -9,9 +8,17 @@
 #include "ESPAsyncWebServer.h"
 #include <SD.h>
 #include <SPIFFS.h>
+#include <NTPClient.h>
 #define CAMERA_MODEL_AI_THINKER     //wybór modelu kamery
 #include "camera_pins.h"
 #define DIODA 33
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+String formattedTime;
+String dayStamp;
+String timeStamp;
 
 const char* ssid = "Lukas";
 const char* password = "nalpinalpi";
@@ -19,8 +26,11 @@ const char* password = "nalpinalpi";
 AsyncWebServer server(80);  //użycie serwera asynchronicznego http na porcie 80
 
 int pictureNumber = 0;
-String pictureList[100];
-String picturePath; 
+int refreshFrequency = 10;
+
+int startHour;
+int endHour;
+bool isWorking = true;
 
 //prosta strona www z miejscem na obraz z kamery
 // const char index_html[] PROGMEM = R"rawliteral(   
@@ -36,23 +46,67 @@ String picturePath;
 
 String index_html() {
   String html = "<!DOCTYPE html> <html>\n";
-    html +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-    html +="<meta http-equiv=\"refresh\" content=\"2;URL='http://esp32.local/'\">";
-    html +="<title>ESP Image Web Server</title>\n";
-    html +="<style>html { font-family: Helvetica; margin: 0px auto; text-align: center;}\n";
-    html +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n</style>\n</head>\n<body>\n";
-    html += "<h2>ESP Image Web Server</h2>\n";
-  for(int i=0; i<pictureNumber; i++) {
-    String pictureEndpoint = pictureList[i];
-    html += "<img src=\"";
-    html += pictureEndpoint + "\">\n";
+  html += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  html += "<meta http-equiv=\"refresh\" content=\"" + String(refreshFrequency) + ";URL='http://esp32.local/'\">";
+  html += "<title>ESP Image Web Server</title>\n";
+  html += "<style>html { font-family: Helvetica; margin: 0px auto; text-align: center;}\n";
+  html += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n</style>\n</head>\n<body>\n";
+  html += "<h2>ESP Image Web Server</h2>\n";
+  html += "<h3>Current time: </3>" + formattedTime + "\n";
+  html += "<div><input type=\"range\" min=\"1\" max=\"20\" value=\"10\" class=\"slider\" id=\"getFrequency\">\n";
+  html += "<p>Reefresh frequency (per second): <span id=\"showFrequency\"></span></p></div>\n";
+  html += "\n";
+  html += "<form action=\"/get\">";
+  html += "From hour (full hour): <input type=\"number\" name=\"fromhHour\">";
+  html += "<input type=\"submit\" value=\"Submit\">";
+  html += "</form><br>";
+  html += "<form action=\"/get\">";
+  html += "To (full hour): <input type=\"number\" name=\"toHour\">";
+  html += "<input type=\"submit\" value=\"Submit\">";
+  html += "</form><br>";
 
-    Serial.println("invoked endpoint for picture: " + pictureEndpoint);
+  if(isWorking) {
+    for(int i=0; i<pictureNumber; i++) {
+      String pictureEndpoint = "/fotka?number=" + String(i+1);
+      html += "<img src=\"" + pictureEndpoint + "\">\n";
+
+      Serial.println("invoked endpoint for picture: " + pictureEndpoint);
+    }
+  } else {
+      html += "<H1>System is working between: " + String(startHour) + " and " + String(endHour) + "hour" + "</H1>";
   }
+
+  if(SPIFFS.totalBytes()-SPIFFS.usedBytes() == 0) {
+    html += "Memory is full";
+  }
+  html += "\n";
+  html += "\n";
+  html += "<script>\n";
+  html += "var slider = document.getElementById(\"getFrequency\");\n";
+  html += "var output = document.getElementById(\"showFrequency\");\n";
+  html += "const req = new XMLHttpRequest();\n";
+  html += "const url='/getFrequency';\n";
+  html += "req.open(\"GET\", url, true);\n";
+  html += "req.send();\n";
+  html += "req.onreadystatechange = (e) => {\n";
+  html += "var frequency = req.responseText;\n";
+  html += "slider.value = frequency;\n";
+  html += "output.innerHTML = frequency;\n";
+  html += "}\n";
+  html += "slider.oninput = function() {\n";
+  html += "output.innerHTML = this.value;\n";
+  html += "sendSlider(this.id, this.value);\n";
+  html += "}\n";
+  html += "function sendSlider(slider, value) {\n";
+  html += "var http = new XMLHttpRequest();\n";
+  html += "var url = '/refresh?frequency=' + value;\n";
+  html += "http.open(\"GET\", url, true);\n";
+  html += "http.send();\n";
+  html += "}\n";
+  html += "</script>\n";
   html += "</body></html>\n";
   return html;
 }
-
 
 
 bool checkPhoto(fs::FS &fs, String photo) {         //funkcja sorawdzająca czy prawidłowo zapisany został plik z obrazem z kamery
@@ -62,9 +116,9 @@ bool checkPhoto(fs::FS &fs, String photo) {         //funkcja sorawdzająca czy 
 }
 
 
- void fotka(String photo)   //funkcja do wykonania zdjęcia 
+ void fotka(int number)   //funkcja do wykonania zdjęcia 
  {
-    photo += ".jpg"; 
+    String photo = "/photo" + String(number) + ".jpg";
     camera_fb_t * fb = NULL;
     bool ok = 0;
   do {
@@ -105,6 +159,7 @@ void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
+  
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0; //definicja portów, do których podłączona jes kamera
   config.ledc_timer = LEDC_TIMER_0;
@@ -163,40 +218,61 @@ if (!SPIFFS.begin(true)) {    //zamontowanie systemu plików w pamięci
   Serial.print("Camera is ready. Go to adress: 'http://");
   Serial.println(WiFi.localIP());
 
-    if (MDNS.begin("esp32")) {
+  if (MDNS.begin("esp32")) {
     Serial.println("MDNS responder started");
   }
-}
-
-void loop() {
-
-  // zapis zdjęcia z nową nazwą z numerem
-  pictureNumber++;
-  String pictureNumberStr = String(pictureNumber);
-  String pictureStr = "/photo";
-  pictureStr += pictureNumberStr;
-  fotka(pictureStr);
-  Serial.println("picture taken properly");
-
-  // dodanie do nazwy zdjecia znumerem do listy zdjęc
-  pictureList[pictureNumber-1] = pictureStr;
-
-  for(int i=0; i<pictureNumber; i++) {
-    String endpointString = pictureList[i];
-    const char * pictureEndpoint = endpointString.c_str(); 
-    picturePath = endpointString + ".jpg";
-
-    Serial.println("created endpoint (server.on): " + endpointString);
-    Serial.println("created picturePath: " + picturePath);
-    server.on(pictureEndpoint, HTTP_GET, [](AsyncWebServerRequest *request) {
-      request->send(SPIFFS, picturePath, "image/jpg");
-    });
-  }
-
+  
+  server.on("/fotka", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String picNumber = request->getParam(0)->value();
+    String picturePath = "/photo" + picNumber + ".jpg";
+    request->send(SPIFFS, picturePath, "image/jpg");
+  });
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(200, "text/html", index_html());
   });
+  server.on("/refresh", HTTP_GET, [](AsyncWebServerRequest *request){
+    String refreshStr = request->getParam(0)->value();
+    refreshFrequency = refreshStr.toInt();
+    request->send(200, "text/html", index_html());
+    Serial.println("Refresh frequncy set to: " +  refreshStr);
+  });
+  server.on("/getFrequency", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", String(refreshFrequency));
+  });
+  
   server.begin();
-  diode_one_blink();
-  delay(10000);
+  pinMode(DIODA,OUTPUT);
+  digitalWrite(DIODA,HIGH);
+
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(3600);
+
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+
+  formattedTime = timeClient.getFormattedTime();
+  Serial.println("Current time: " + formattedTime);
+}
+
+void loop() {
+  if(SPIFFS.totalBytes()-SPIFFS.usedBytes() == 0) {
+    Serial.println("SPIFFS memory is full");
+  } else {
+  formattedTime = timeClient.getFormattedTime();
+  if(pictureNumber == 10) {
+    pictureNumber = 0;
+    }
+    pictureNumber++;
+    fotka(pictureNumber);
+    diode_one_blink();
+    Serial.println();
+    Serial.println("Invoking delay for: " + String(refreshFrequency));
+    delay(refreshFrequency*1000);
+  }
 }
